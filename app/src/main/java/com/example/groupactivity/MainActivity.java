46a9a9f2ef8f06ce3eccf0ca6d4e5f3699e9938a
tejas5545 <tejasbhut5545@gmail.com>
2020-08-02 15:04:55 +0530

    AlertDialog Added

--- app/src/main/java/com/example/groupactivity/MainActivity.java
+++ app/src/main/java/com/example/groupactivity/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.groupactivity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     RelativeLayout rellay1,rellay2;
     Handler splash=new Handler();
     EditText edusn,edpass;
+    Button btnlogin;
     Runnable r=new Runnable() {
         @Override
         public void run() {
@@ -29,24 +34,44 @@ public class MainActivity extends AppCompatActivity {
         rellay2=findViewById(R.id.rellay2);
         edusn=findViewById(R.id.username);
         edpass=findViewById(R.id.password);
-        splash.postDelayed(r,5000);
-        if(edusn.getText().toString().equals("TejasPatel") && edpass.getText().toString().equals("123456"))
-        {
-            Intent i=new Intent(getApplicationContext(),AboutUs.class);
-            startActivity(i);
-            finish();
-        }
-        else if(edusn.getText().toString().equals("KoshaPatel") && edpass.getText().toString().equals("123456"))
-        {
-            Intent i=new Intent(getApplicationContext(),AboutUs.class);
-            startActivity(i);
-            finish();
-        }
-        else if(edusn.getText().toString().equals("ManjulaOdedra") && edpass.getText().toString().equals("123456"))
-        {
-            Intent i=new Intent(getApplicationContext(),AboutUs.class);
-            startActivity(i);
-            finish();
-        }
+        btnlogin=findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(edusn.getText().toString().equals("TejasPatel") && edpass.getText().toString().equals("123456"))
+                {
+                    Intent i=new Intent(getApplicationContext(),AboutUs.class);
+                    startActivity(i);
+                    finish();
+                }
+                else if(edusn.getText().toString().equals("KoshaPatel") && edpass.getText().toString().equals("123456"))
+                {
+                    Intent i=new Intent(getApplicationContext(),AboutUs.class);
+                    startActivity(i);
+                    finish();
+                }
+                else if(edusn.getText().toString().equals("ManjulaOdedra") && edpass.getText().toString().equals("123456"))
+                {
+                    Intent i=new Intent(getApplicationContext(),AboutUs.class);
+                    startActivity(i);
+                    finish();
+                }
+                else
+                {
+                    new AlertDialog.Builder().setMessage("Invalid Credential").setPositiveButton("Try", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    }).setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    }).create().show();
+                }
+            }
+        });
+
     }
 }
