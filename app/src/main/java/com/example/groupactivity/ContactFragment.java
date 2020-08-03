d9582cffe2479522291119e703a638dc61b8280c
tejas5545 <tejasbhut5545@gmail.com>
2020-08-03 09:43:10 +0530

    AlertDialog Added

--- app/src/main/java/com/example/groupactivity/ContactFragment.java
+++ app/src/main/java/com/example/groupactivity/ContactFragment.java
@@ -1,25 +1,48 @@
 package com.example.groupactivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ContactFragment extends Fragment {
+    Context context;
 
 
     public ContactFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final EditText edname=view.findViewById(R.id.fname);
+        final EditText eml=view.findViewById(R.id.emls);
+        final EditText mob=view.findViewById(R.id.mn);
+        final EditText msgs=view.findViewById(R.id.msg);
+        Button btnsub=view.findViewById(R.id.btnsend);
+        btnsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Sending function goes here",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
